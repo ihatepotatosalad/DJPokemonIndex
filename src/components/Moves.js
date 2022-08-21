cb8e71d6c7cc7af4769994e646c3e2d47d3962c8
ihatepotatosalad <101296339+ihatepotatosalad@users.noreply.github.com>
2022-08-21 13:42:56 -0400

    all linkes created and functional

---
+++ src/components/Moves.js
@@ -0,0 +1,11 @@
+import { React, useEffect, useState } from "react"
+export default function Moves() {
+    return (
+        <div className="aboutpage">
+
+            <h1>Moves Page</h1>
+
+
+        </div>
+    )
+}
---
+++ src/components/Pokemon.js
@@ -0,0 +1,11 @@
+import { React, useEffect, useState } from "react"
+export default function Pokemon() {
+    return (
+        <div className="aboutpage">
+            <div className="aboutHeader">
+                <h1>Pokemon Page</h1>
+            </div>
+            <h1>Pokemon Page</h1>
+        </div>
+    )
+}
